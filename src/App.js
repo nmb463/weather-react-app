440e40c8773bfe71f4f4860929f8298219b8b1cd
Nicole Banda <nmbanda124@gmail.com>
2024-11-10 14:24:06 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1>React Weather Search</h1>
-      <Weather />
-      <div className="Links">
-        <a href="https://github.com/nmb463/weather-react-app" target="_blank" rel="noreferrer">Open-source code</a> by Nicole Banda
+      <div className="Container">
+        <h1>React Weather Search</h1>
+        <Weather />
+        <div className="Links">
+          <a href="https://github.com/nmb463/weather-react-app" target="_blank" rel="noreferrer">Open-source code</a> by Nicole Banda
+        </div>
       </div>
     </div>
   );
