780b43004b6fb2dfe3d145f25c998394832da7c9
Nicole Banda <nmbanda124@gmail.com>
2024-11-12 16:34:58 -0800

    Created the layout and styling for one day within the forecast

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 
 
@@ -51,6 +52,7 @@ export default function Weather(props) {
                     </div>
                 </form>
                 <WeatherInfo data={weatherData} />
+                <WeatherForecast code={weatherData.icon} />
             </div>
         )
     } else {
